cac6434f1e247c6c64a0ebd93cc75d1090b1ce39
Nitin Bawane <nitinbawane86@gmail.com>
2020-03-16 19:22:58 +0530

    remove weather search component
    remove weather search component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   appId: '45f4dd45e0f724512ba044c5a2caf4bc',
   baseUrl: 'http://api.openweathermap.org/data/2.5/',
   units: 'imperial'
